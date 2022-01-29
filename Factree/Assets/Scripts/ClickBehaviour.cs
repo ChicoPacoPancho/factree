102aca88815b0ef663835f6c8a1e88564fe7875f
Mykyta Shvets <nikitashets28@gmail.com>
2022-01-29 09:51:25 -0600

    Added a selection square as well as pixel camera

--- Factree/Assets/Scripts/ClickBehaviour.cs
+++ Factree/Assets/Scripts/ClickBehaviour.cs
@@ -9,6 +9,7 @@ public class ClickBehaviour : MonoBehaviour
     public Tilemap grid;
     public Tile setTo;
     public GameObject follow;
+    public GameObject selectionSquare;
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +20,20 @@ public class ClickBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Get the position of the mouse and convert it to cells
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0; // By default the function above returns -10 which is not right
+        Vector3Int cell = grid.WorldToCell(pos);
+
+        Vector3 roundedPos = grid.CellToWorld(cell);
+        selectionSquare.transform.position = roundedPos;
+
         if (Input.GetMouseButtonDown(0))
         { 
-            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            pos.z = 0;
-            follow.transform.position = pos; 
-            Vector3Int cell = grid.WorldToCell(pos);
             Debug.Log(cell);
             Debug.Log(grid.GetTile(cell));
+            follow.transform.position = pos;
             grid.SetTile(cell, setTo);
-            grid.RefreshAllTiles();
         }
     }
 }
