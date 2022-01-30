17f39a8b77952928dead8d5df1934208c4d9f450
Rene Dufour-Contreras <rene.cpp@gmail.com>
2022-01-30 12:44:06 -0600

    Add functionality to giant fly trap

---
+++ Factree/Assets/Scripts/AbilityManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityManager : MonoBehaviour
+{
+    public List<CollectResourceAbility> ongoingAbilities;
+
+    void Start()
+    {
+        
+    }
+}
